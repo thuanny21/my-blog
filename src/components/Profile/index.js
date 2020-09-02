eeeb65ff7a51d9046fee734db78d22b09d7c2ec7
Thuanny <thucatm@gmail.com>
2020-09-02 20:28:25 -0300

    criando componente e compartilhando entre páginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Thuanny</h1>
+        <h2>Desenvolvedora Frontend</h2>
+        <p>Ola sou a Thuanny desenvolvedora frontend e outras coisas a mais.</p>
+    </div>
+)
+
+export default Profile
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,14 @@
 import React from "react"
-import { Link } from 'gatsby'
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
-    <ul>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <Link to="/" activeStyle={ { color: 'red'}}>Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
   </Layout>
 )
 
