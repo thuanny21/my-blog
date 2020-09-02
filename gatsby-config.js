a2750eb21a32423cfe962cc9437416449ee57b62
Thuanny <thucatm@gmail.com>
2020-09-02 19:51:35 -0300

    limpar coisas para startar projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Blog`,
+    description: `Blog sobre frontend development.`,
+    author: `@thuannytm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
