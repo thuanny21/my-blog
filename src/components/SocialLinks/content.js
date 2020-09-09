fc3d86f93085c2f76a4557e58884a2b3a929883e
Thuanny <thucatm@gmail.com>
2020-09-09 19:19:36 -0300

    criando social links part 1

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/thuanny21'
+    }
+    {
+        label: 'Twitter',
+        url: 'https://github.com/thuanny21'
+    }
+    {
+        label: 'Youtube',
+        url: 'https://github.com/thuanny21'
+    }
+    {
+        label: 'Instagram',
+        url: 'https://github.com/thuanny21'
+    }
+    {
+        label: 'Unsplash',
+        url: 'https://github.com/thuanny21'
+    }
+]
+
+export default links
